a10ca3c4471cd9f19b36c261b4582338f0323f06
Enis Kurtay YILMAZ <kurtay@codeapp.co>
2021-10-28 00:00:44 +0300

    upgraded softwareDownloadURL

--- src/app/_viewmodel/software/software.ts
+++ src/app/_viewmodel/software/software.ts
@@ -1,6 +1,7 @@
 export class Software {
     softwareId: 1;
     softwareName: string;
+    softwareDownloadUrl: string;
     softwareLastVersion: string;
     softwareIsActive: boolean;
     createdDateTime: string;
